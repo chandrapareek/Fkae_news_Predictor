3b375eeab76a328b5c6e8cc3d60ef3c0352ae740
Chandra Pareek <pareekchandra10@gmail.com>
2022-08-10 16:47:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loanpredictor/false_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.loanpredictor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class false_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.false_activity)
+    }
+    }
